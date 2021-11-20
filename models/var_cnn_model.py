43f2c8967cfa45078fdb96f81111a3bd41bd7140
Neriya Mazzuz <neriya92@gmail.com>
2021-11-20 21:12:44 +0200

    Add var_cnn

---
+++ models/var_cnn_model.py
@@ -0,0 +1,131 @@
+from keras.models import Model
+from keras.layers.convolutional import Conv1D, MaxPooling1D
+from keras.layers import Dense, Activation, ZeroPadding1D, \
+    GlobalAveragePooling1D, Add, Concatenate
+from tensorflow.keras.layers import BatchNormalization
+from keras import Input
+
+parameters = {'kernel_initializer': 'he_normal'}
+
+
+def dilated_basic_1d(filters, suffix, stage=0, block=0, kernel_size=3,
+                     numerical_name=False, stride=None,
+                     dilations=(1, 1)):
+    """A one-dimensional basic residual block with dilations.
+    :param filters: the output’s feature space
+    :param stage: int representing the stage of this block (starting from 0)
+    :param block: int representing this block (starting from 0)
+    :param kernel_size: size of the kernel
+    :param numerical_name: if true, uses numbers to represent blocks instead of chars (ResNet{101, 152, 200})
+    :param stride: int representing the stride used in the shortcut and the first conv layer, default derives stride from block id
+    :param dilations: tuple representing amount to dilate first and second conv layers
+    """
+    if stride is None:
+        if block != 0 or stage == 0:
+            stride = 1
+        else:
+            stride = 2
+
+    if block > 0 and numerical_name:
+        block_char = 'b{}'.format(block)
+    else:
+        block_char = chr(ord('a') + block)
+
+    stage_char = str(stage + 2)
+
+    def f(x):
+        y = Conv1D(filters, kernel_size, padding='causal', strides=stride,
+                   dilation_rate=dilations[0], use_bias=False,
+                   name='res{}{}_branch2a_{}'.format(
+                       stage_char, block_char, suffix), **parameters)(x)
+        y = BatchNormalization(epsilon=1e-5,
+                               name='bn{}{}_branch2a_{}'.format(
+                                   stage_char, block_char, suffix))(y)
+        y = Activation('relu',
+                       name='res{}{}_branch2a_relu_{}'.format(
+                           stage_char, block_char, suffix))(y)
+
+        y = Conv1D(filters, kernel_size, padding='causal', use_bias=False,
+                   dilation_rate=dilations[1],
+                   name='res{}{}_branch2b_{}'.format(
+                       stage_char, block_char, suffix), **parameters)(y)
+        y = BatchNormalization(epsilon=1e-5,
+                               name='bn{}{}_branch2b_{}'.format(
+                                   stage_char, block_char, suffix))(y)
+
+        if block == 0:
+            shortcut = Conv1D(filters, 1, strides=stride, use_bias=False,
+                              name='res{}{}_branch1_{}'.format(
+                                  stage_char, block_char, suffix),
+                              **parameters)(x)
+            shortcut = BatchNormalization(epsilon=1e-5,
+                                          name='bn{}{}_branch1_{}'.format(
+                                              stage_char, block_char,
+                                              suffix))(shortcut)
+        else:
+            shortcut = x
+
+        y = Add(name='res{}{}_{}'.format(stage_char, block_char, suffix))(
+            [y, shortcut])
+        y = Activation('relu',
+                       name='res{}{}_relu_{}'.format(stage_char, block_char,
+                                                     suffix))(y)
+
+        return y
+
+    return f
+
+
+# Code for standard ResNet model is based on
+# https://github.com/broadinstitute/keras-resnet
+def ResNet18(inputs, suffix, blocks=None, block=None, numerical_names=None):
+    """Constructs a `keras.models.Model` object using the given block count.
+    :param inputs: input tensor (e.g. an instance of `keras.layers.Input`)
+    :param blocks: the network’s residual architecture
+    :param block: a residual block (e.g. an instance of
+        `keras_resnet.blocks.basic_2d`)
+    :param numerical_names: list of bool, same size as blocks, used to
+        indicate whether names of layers should include numbers or letters
+    :return model: ResNet model with encoding output (if `include_top=False`)
+        or classification output (if `include_top=True`)
+    """
+    if blocks is None:
+        blocks = [2, 2, 2, 2]
+    if block is None:
+        block = dilated_basic_1d
+    if numerical_names is None:
+        numerical_names = [True] * len(blocks)
+
+    x = ZeroPadding1D(padding=3, name='padding_conv1_' + suffix)(inputs)
+    x = Conv1D(64, 7, strides=2, use_bias=False, name='conv1_' + suffix)(x)
+    x = BatchNormalization(epsilon=1e-5, name='bn_conv1_' + suffix)(x)
+    x = Activation('relu', name='conv1_relu_' + suffix)(x)
+    x = MaxPooling1D(3, strides=2, padding='same', name='pool1_' + suffix)(x)
+
+    features = 64
+    outputs = []
+
+    for stage_id, iterations in enumerate(blocks):
+        x = block(features, suffix, stage_id, 0, dilations=(1, 2),
+                  numerical_name=False)(x)
+        for block_id in range(1, iterations):
+            x = block(features, suffix, stage_id, block_id, dilations=(4, 8),
+                      numerical_name=(
+                              block_id > 0 and numerical_names[stage_id]))(
+                x)
+
+        features *= 2
+        outputs.append(x)
+
+    x = GlobalAveragePooling1D(name='pool5_' + suffix)(x)
+    return x
+
+
+def var_cnn(input_shape, emb_size):
+    dir_input = Input(shape=input_shape, name='dir_input')
+    dir_output = ResNet18(dir_input, 'dir', block=dilated_basic_1d)
+    model_output = Dense(emb_size, name='model_output')(dir_output)
+    return Model(inputs=dir_input, outputs=model_output)
+
+
+
